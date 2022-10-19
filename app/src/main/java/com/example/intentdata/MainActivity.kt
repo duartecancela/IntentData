e6a91bdc35e3c6d79275f7b78055b4ba935646c7
Duarte Cancela <duartecancela@gmail.com>
2022-10-19 12:47:17 +0100

    update code

--- app/src/main/java/com/example/intentdata/MainActivity.kt
+++ app/src/main/java/com/example/intentdata/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 
@@ -12,12 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textViewPersonName = findViewById<TextView>(R.id.textViewPersonName)
         val buttonEdit = findViewById<Button>(R.id.buttonEdit)
+
 
         buttonEdit.setOnClickListener {
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-
+                if(it.resultCode == RESULT_OK) {
+                    var data = it.data
+                    val name = data?.getStringExtra("name")
+                    textViewPersonName.text = name
+                }
             }.launch(Intent(this, FormActivity::class.java))
         }
 
